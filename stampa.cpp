f41d1f61c85b74a16a9bced651f1ee942f51c2bb
Rida <carola.rida@itismeucci.com>
2023-03-30 08:44:10 +0200

    errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -1,19 +1,20 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+RIDA CAROLA 30/03/2023
 */
 
 # include <iostream>
 using namespace std;
 int main()
 {
-  int i=10 ,n;
+  int i=0 ,n;
   cout<<"Quante volte voi stampare la frase? ";
   cin>>n;
   while (i<n)
   {
    cout<<"Bada come la fuma"<<endl; 
+   i++;
   }
   system("pause");
 }
